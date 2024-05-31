fa4c9ff2e32e908b1e0ced80a1c72a0f81f81843
AlexaCabral <amcabral2@up.edu.ph>
2024-05-31 22:07:46 +0800

    images added

--- account/loginAccount.py
+++ account/loginAccount.py
@@ -4,6 +4,11 @@ import mysql.connector #fix error: pip install mysql-connector-python
 
 root = tk.Tk()
 
+# window elements
+root.geometry("1100x650")
+root.title("Login")
+root.config(bg="#cccccc")
+
 
 
 root.mainloop()
