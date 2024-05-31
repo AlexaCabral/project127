9f145567bb79f9d6a7d7455a6f7d63f7cca162ba
AlexaCabral <amcabral2@up.edu.ph>
2024-05-31 21:39:48 +0800

    database file

---
+++ account/project.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS project;
+
+CREATE OR REPLACE USER 'project'@'localhost' IDENTIFIED BY 'ilove127';
+CREATE DATABASE project;
+GRANT ALL ON scott.* TO 'project'@'localhost';
+
+USE project;
+
+CREATE TABLE customer(
+    account_id INT(8) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	password VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+    CONSTRAINT email_email_valid CHECK (email LIKE '%@%.%'),
+	CONSTRAINT customer_email_uk UNIQUE(email),
+	PRIMARY KEY(account_id) 
+);
+
+-- TEST DATA
+-- error
+INSERT INTO customer VALUES(1, "customer", "password", "customer"); 
+--valid
+INSERT INTO customer VALUES(1, "customer", "password", "customer@gugol.kom"); 
