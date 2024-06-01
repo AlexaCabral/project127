86791ccd3a4bfab262e72f62998ca30ec5db7bcf
AlexaCabral <amcabral2@up.edu.ph>
2024-06-01 13:28:52 +0800

    DCL: owner table added

--- account/project.sql
+++ account/project.sql
@@ -3,6 +3,29 @@ DROP DATABASE IF EXISTS project;
 CREATE DATABASE project;
 USE project;
 
+CREATE TABLE owner(
+    account_id INT(8) NOT NULL AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	password VARCHAR(50) NOT NULL,
+    CONSTRAINT owner_password_valid
+    CHECK (
+        (INSTR(password, '0') > 0) OR
+        (INSTR(password, '1') > 0) OR
+        (INSTR(password, '2') > 0) OR
+        (INSTR(password, '3') > 0) OR
+        (INSTR(password, '4') > 0) OR
+        (INSTR(password, '5') > 0) OR
+        (INSTR(password, '6') > 0) OR
+        (INSTR(password, '7') > 0) OR
+        (INSTR(password, '8') > 0) OR
+        (INSTR(password, '9') > 0)
+    ),
+	email VARCHAR(50) NOT NULL,
+    CONSTRAINT owner_email_valid CHECK (email LIKE '%_%@%_%.%_%'),
+	CONSTRAINT owner_email_uk UNIQUE(email),
+	PRIMARY KEY(account_id) 
+);
+
 CREATE TABLE customer(
     account_id INT(8) NOT NULL AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
